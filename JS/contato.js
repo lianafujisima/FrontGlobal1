1b822248b78214dde51439af59fa9a77741c0f48
lianafujisima <lianalmf@hotmail.com>
2025-05-28 19:01:43 -0300

    sexto commit

---
+++ JS/contato.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const botaoMostrar = document.getElementById("botaomostrar");
+    const secaoMostrar = document.getElementById("smostrar");
+  
+    if (botaoMostrar && secaoMostrar) {
+      botaoMostrar.addEventListener("click", function () {
+        if (secaoMostrar.style.display === "none") {
+          secaoMostrar.style.display = "block";
+        } else {
+          secaoMostrar.style.display = "none";
+        }
+      });
+    }
+  });
