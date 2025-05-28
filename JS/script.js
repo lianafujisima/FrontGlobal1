c5367e80da53b1b0fc6517c2e45a82f0614b6ed0
lianafujisima <lianalmf@hotmail.com>
2025-05-28 20:36:50 -0300

    setimo commit

--- JS/script.js
+++ JS/script.js
@@ -28,18 +28,3 @@ document.getElementById('cadastroForm').addEventListener('submit', function(even
 
 // ===== Mostrar/Ocultar uma seção =====
 
-
-document.addEventListener("DOMContentLoaded", function () {
-    const botaoMostrar = document.getElementById("botaomostrar");
-    const secaoMostrar = document.getElementById("smostrar");
-  
-    if (botaoMostrar && secaoMostrar) {
-      botaoMostrar.addEventListener("click", function () {
-        if (secaoMostrar.style.display === "none") {
-          secaoMostrar.style.display = "block";
-        } else {
-          secaoMostrar.style.display = "none";
-        }
-      });
-    }
-  });
