78cd527b9fee7e2628ce99bfa54dc0066c07b14c
Victorcho05 <victorhwancho05@gmail.com>
2025-05-30 09:29:52 -0300

    11commit

---
+++ FrontGlobal1/FrontGlobal1-main/JS/sos.js
@@ -0,0 +1,5 @@
+function acionarSocorro() {
+    const mensagem = document.getElementById('sosmensagem');
+
+    mensagem.innerText = 'Vamos compartilhar sua localização com as autoridades. Aguarde socorro!';
+  }
